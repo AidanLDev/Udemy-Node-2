6964ee0ac9d18e070164ca85fd77a9279738168b
Aidan <aidansoftwaredev@gmail.com>
2019-09-21 15:10:06 +0100

    Cart view and logic

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,72 +1,94 @@
-const Product = require('../models/product');
-const Cart = require('../models/cart');
-
+const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 //  GET /products
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop/product-list', {
+    res.render("shop/product-list", {
       prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+      pageTitle: "All Products",
+      path: "/products"
     });
   });
-}
+};
 
 //  GET /products/:productId
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
 
   Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
+    res.render("shop/product-detail", {
       pageTitle: product.title,
       product: product,
-      path: '/products'
+      path: "/products"
     });
   });
-  
-}
+};
 
 //  GET /
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop/index', {
+    res.render("shop/index", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+      pageTitle: "Shop",
+      path: "/"
     });
   });
-}
+};
 
 //  GET /cart
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    pageTitle: 'Your Cart',
-    path: '/cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      //  Only get products in the cart
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
+        products: cartProducts
+      });
+    });
   });
 };
 
 //  POST /cart
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
+  Product.findById(prodId, product => {
     Cart.addProduct(prodId, product.price);
   });
-  res.redirect('/cart')
+  res.redirect("/cart");
 };
 
 //  GET /orders
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    pageTitle: 'Your Orders',
-    path: '/orders'
-  })
-}
+  res.render("shop/orders", {
+    pageTitle: "Your Orders",
+    path: "/orders"
+  });
+};
 
 //  GET /checkout
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    title: 'Checkout',
-    path: '/checkout'
+  res.render("shop/checkout", {
+    title: "Checkout",
+    path: "/checkout"
   });
-}
+};
+
+//  POST /cart-delete-item
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,26 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const shopController = require('../controllers/shop');
+const shopController = require("../controllers/shop");
 
 //  GET /
-router.get('/', shopController.getIndex);
+router.get("/", shopController.getIndex);
 
 //  GET /products
-router.get('/products', shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
 //  GET /products/:productId (colon signals to express it should look for a variable)
-router.get('/products/:productId', shopController.getProduct)
+router.get("/products/:productId", shopController.getProduct);
 
 //  GET /cart
-router.get('/cart', shopController.getCart);
+router.get("/cart", shopController.getCart);
 
 //  POST /cart
-router.post('/cart', shopController.postCart);
+router.post("/cart", shopController.postCart);
 
 //  GET /orders
-router.get('/orders', shopController.getOrders);
+router.get("/orders", shopController.getOrders);
 
 //  GET /checkout
-router.get('/checkout', shopController.getCheckout);
+router.get("/checkout", shopController.getCheckout);
+
+//  POST /cart-delete-item
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 module.exports = router;
